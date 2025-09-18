c73f40b9336f1133d84862eba0b8709fbbc85ed7
nomayen31 <nomayen.ohin@gmail.com>
2025-09-18 10:06:05 +0600

    first commit

---
+++ src/Layout/MainLayouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../Components/NavBar/NavBar";
+import Hero from "../../Components/Hero/Hero";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero/>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Router/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayouts/MainLayout';
+import Home from '../Pages/Home/Home';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>
+            }
+        ]
+    }
+])
+
+
+export default router;
